803bf47fed392ef7eb75d9d4de1d808554d3dfcf
Alan Cristian <alan.dias@dcomp.ufs.br>
2026-02-10 20:58:59 -0300

    Update ddxquestao5.c
    alterações nos endereços dos arquivos.

--- Principais/ddxquestao5.c
+++ Principais/ddxquestao5.c
@@ -64,7 +64,8 @@ int executarDdxQuestao5(){
         }
     }
 
-    FILE *resultados = abrirArquivoDados("results.csv","r");//pega os arquivos com todos os jogos.
+    //está na paste "clean-data"
+    FILE *resultados = abrirArquivoDados("clean-data/results.csv","r");//pega os arquivos com todos os jogos.
 
     //agora precisamos fazer uma lista dinâmica para armazenar todos os ID's dos atletas que ganharma uma olípiada em determinado ano.
     int *lista = NULL; // aqui estarão os ids dos ganhadores de medalhas em determinado ano.
@@ -135,7 +136,8 @@ int executarDdxQuestao5(){
         }
     }
 
-    FILE *bio_genero = abrirArquivoDados("bios.csv","r");
+    //está na pasta "athletes"
+    FILE *bio_genero = abrirArquivoDados("athletes/bios.csv","r");
 
     fgets(linha, MAX_LINE, bio_genero);//apenas para remover o cabeçalho.
 
@@ -211,7 +213,8 @@ int executarDdxQuestao5(){
     //agora iremos calcular o peso médio de cada gênero do esporte em determinado ano.
 
     //esse arquivo contme os pesos de todos os atletas por id.
-    FILE *pesos = abrirArquivoDados("bios_peso.csv", "r");
+    //está na paste "clean-data"
+    FILE *pesos = abrirArquivoDados("clean-data/bios.csv", "r");
 
     fgets(linha, MAX_LINE, pesos);//apenas para remover o cabeçalho.
 
